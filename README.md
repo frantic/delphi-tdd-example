133deb4e8960876be741aaddfe97b12aa4426201
Alexander Kotlyarskiy <alex.frantic@gmail.com>
2012-02-28 15:15:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Delphi TDD Workflow Example
+
+RSSReader is simple example of Delphi application built using TDD. For more information visit http://delphi.frantic.im/
+
+LICENSE - MIT
+
+(c) 2012 Alexander Kotlyarskiy
---
+++ RSSReader.dpr
@@ -0,0 +1,14 @@
+program RSSReader;
+
+uses
+  Forms,
+  Unit1 in 'src\Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
