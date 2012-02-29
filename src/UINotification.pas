975ed85c94ea313b64dda9ec0823358a7090011d
Alexander Kotlyarskiy <alex.frantic@gmail.com>
2012-02-29 16:25:28 +0200

    Fixed interface name spelling

--- src/UINotification.pas
+++ src/UINotification.pas
@@ -6,14 +6,14 @@ uses
   RssModel, SysUtils;
 
 type
-  UINotificationService = interface
+  IUINotificationService = interface
     ['{A8E7E1F7-E60F-465A-9D8C-0B6186CA7A06}']
     procedure Notify(Item: TRSSItem);
     procedure NotifyError(E: Exception);
   end;
 
 var
-  DefaultUINotification: UINotificationService = nil;
+  DefaultUINotification: IUINotificationService = nil;
 
 implementation
 
